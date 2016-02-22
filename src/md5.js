c05e4eb5fdf18a1d97d4c3224413f5d315def1a8
Anson Chen <anson@cht.com.tw>
2016-02-22 15:16:44 +0800

    Setup eslint, clean up code with js standard style and restructure package.json.

--- src/md5.js
+++ src/md5.js
@@ -1,13 +1,13 @@
-  "use strict"
-  import crypto from 'crypto'
-  import fs from 'fs'
-  const filename = process.argv[2],
-	shasum = crypto.createHash('md5'),
-	s = fs.ReadStream(filename)
+'use strict'
+import crypto from 'crypto'
+import fs from 'fs'
+const filename = process.argv[2]
+const shasum = crypto.createHash('md5')
+const s = fs.ReadStream(filename)
 
-  s.on('data', d => shasum.update(d))
+s.on('data', (d) => shasum.update(d))
 
-  s.on('end', () => {
-    const d = shasum.digest('hex')
-    console.log(`${d} ${filename}`)
-  });
+s.on('end', () => {
+  const d = shasum.digest('hex')
+  console.log(`${d} ${filename}`)
+})
