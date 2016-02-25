014ef219c4ba58940dca62e26fc3d3095b07bbd1
Anson Chen <anson@cht.com.tw>
2016-02-25 15:16:37 +0800

    Remove redundent variable

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@
 import md5 from './md5'
 import fs from 'fs'
 const filename = process.argv[2]
-const s = fs.ReadStream(filename)
 
-md5(s).then((sum) => {
+md5(fs.ReadStream(filename)).then((sum) => {
   console.log(`MD5 sum of "${filename}" is ${sum}`)
 })
--- test/test_md5.js
+++ test/test_md5.js
@@ -2,13 +2,13 @@
 
 import md5 from '../lib/md5'
 import fs from 'fs'
-const filename = './test/TEST_INPUT'
-const s = fs.ReadStream(filename)
 import { expect } from 'chai'
+
+const filename = './test/TEST_INPUT'
 
 describe('MD5', () => {
   it('should sum md5 correctly', (done) => {
-    md5(s).then((sum) => {
+    md5(fs.ReadStream(filename)).then((sum) => {
       expect(sum).to.equal('b711b1aad4df9f6e7f5b511531cc5077')
       done()
     })
