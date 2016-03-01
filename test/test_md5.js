14eb353cfaf2d240e1b1b2a7c3dccb5f614bd50a
Anson Chen <anson@cht.com.tw>
2016-03-01 16:21:39 +0800

    Rewrite mocha test case: 1. use normal function instead of arrow function to prevent this binding. 2. use async function to simplfy test

--- test/test_md5.js
+++ test/test_md5.js
@@ -6,12 +6,9 @@ import { expect } from 'chai'
 
 const filename = './test/TEST_INPUT'
 
-describe('MD5', () => {
-  it('should sum md5 correctly', (done) => {
-    md5(fs.ReadStream(filename)).then((sum) => {
-      expect(sum).to.equal('b711b1aad4df9f6e7f5b511531cc5077')
-      done()
-    })
+describe('MD5', function () {
+  it('should sum md5 correctly', async function () {
+    const sum = await md5(fs.ReadStream(filename))
+    expect(sum).to.equal('b711b1aad4df9f6e7f5b511531cc5077')
   })
 })
-
